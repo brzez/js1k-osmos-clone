cdeb221319fd36fae1f01421fcfb494e9c68a32a
brzez <pawel@brzez.com>
2017-02-07 08:28:10 +0100

    spawning new entities

--- demo.js
+++ demo.js
@@ -7,13 +7,20 @@ var all = [];
 var w = a.width;
 var h = a.height;
 
+var rng = Math.random;
+
 function make(t){
-    var o = {x:w/2, y:h/2, r: 8, t: t||1, vx: 0, vy: 0};
+    var o = {x: rng() * w, y: rng() * h, r: 8, t: t||1, vx: 0, vy: 0};
     all.push(o);
     return o;
 }
 
+// todo: move to setinterval
 function update(o){
+    if(o.t==1 && rng() > .99){
+        o.vx = rng() * 2 - 1;
+        o.vy = rng() * 2 - 1;
+    }
     o.x += o.vx;
     o.y += o.vy;
     c.fillRect(o.x, o.y, 8, 8);
@@ -28,6 +35,8 @@ setInterval(function(){
     c.restore();
     for(var i=all.length;i-->0;){
         update(all[i]);
+
+        if(all.length < 10){make();}
     }
 
     b.onmousemove = function(e) {
